a28bc525dedb0bb97bac3dac3cf198ba9d7ca4c0
Bunceanu Gabriel <bunceanugabryel@gmail.com>
2020-10-21 00:27:18 +0300

    Rezolvare Tema4

---
+++ Bunceanu Gabriel-Daniel/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionResult.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp.Choices;
+using System.Dynamic;
+using System.Linq;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    [AsChoice]
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult { }
+
+        public class QuestionCreated : ICreateQuestionResult
+        {
+            public Guid Question_ID { get; private set; }
+            public string Email { get; private set; }
+            public string Question { get; private set; }
+            public bool ApprovedbyML { get; set; }
+
+
+            public QuestionCreated(Guid Q_ID, string question, string email, bool ML_Approved)
+            {
+                Question_ID = Q_ID;
+                Question = question;
+                Email = email;
+                ApprovedbyML = ML_Approved;
+            }
+        }
+        public class QuestionNotCreated : ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class QuestionValidationFailed : ICreateQuestionResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+}
---
+++ Bunceanu Gabriel-Daniel/L04/Test.App/TestQuestion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Question.Domain.CreateQuestionWorkflow;
+using static Question.Domain.CreateQuestionWorkflow.CreateQuestionResult;
+
+namespace Test.App
+{
+    public class TestQuestion
+    {
+        public static void QuestionMain()
+        {
+            var cmd = new CreateQuestionCmd("Test Title", "Test Body", new string[] {"c#","python", "c++","java"});
+            var result = CreateQuestion(cmd);
+            result.Match(
+                ProcessQuestionCreated,
+                ProcessQuestionNotCreated,
+                ProcessInvalidQuestion
+                );
+            Console.ReadLine();
+        }
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+        private static ICreateQuestionResult ProcessQuestionNotCreated(QuestionNotCreated questionNotCreatedResult)
+        {
+            Console.WriteLine($"Question not created: {questionNotCreatedResult.Reason}");
+            return questionNotCreatedResult;
+        }
+        private static ICreateQuestionResult ProcessQuestionCreated(QuestionCreated question)
+        {
+            Console.WriteLine($"Question {question.Question_ID}");
+            return question;
+        }
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if (string.IsNullOrWhiteSpace(createQuestionCommand.Title) || string.IsNullOrWhiteSpace(createQuestionCommand.Body))
+            {
+                var errors = new List<string>() { "Invalid title or description" };
+                return new QuestionValidationFailed(errors);
+            }
+            if (new Random().Next(3) > 1)
+            {
+                return new QuestionNotCreated("Question could not be verified");
+            }
+            var questionId = Guid.NewGuid();
+            var results = new QuestionCreated(questionId, createQuestionCommand.Title, "test.bunceanu@gmail.com", true);
+            return results;
+        }
+    }
+}
